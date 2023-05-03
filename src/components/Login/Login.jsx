92dc19dd4c06984d74df464d2f6aa31f1d4bb6ce
Md. Tarek Hasan <tarekhasan.tkg101@gmail.com>
2023-05-03 09:43:22 +0600

    navigation done from login page to homepage after a successful login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,11 +1,12 @@
 import React, { useContext } from "react";
 import "./Login.css";
 import { Button, Container, Form } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../providers/AuthProvider";
 
 const Login = () => {
     const { signIn } = useContext(AuthContext);
+    const navigate = useNavigate();
 
 
     const handleLogin = event =>{
@@ -19,6 +20,7 @@ const Login = () => {
         .then(result =>{
             const loggedUser = result.user;
             console.log(loggedUser);
+            navigate('/');
         })
         .catch(error =>{
             console.log(error);
